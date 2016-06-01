d0c5248f52572490925a6ca5c992198b89c8b79c
HUB <andreas.huebner@uniklinik-freiburg.de>
2016-06-01 16:20:43 +0200

    v 1.0.5.2

--- src/AdtGekid/Properties/AssemblyInfo.cs
+++ src/AdtGekid/Properties/AssemblyInfo.cs
@@ -28,9 +28,9 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("AdtGekid")]
-[assembly: AssemblyDescription("Entitäten für den ADT_GEKID Basisdatensatz Version 1.0.5")]
+[assembly: AssemblyDescription("German Project. Entitäten für den ADT_GEKID Basisdatensatz Version 1.0.5")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Andreas Hübner")]
 [assembly: AssemblyProduct("AdtGekid")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -40,5 +40,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("4fec61bd-d46d-46a1-a3e1-3a94255bd443")]
 
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyInformationalVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.5.2")]
+[assembly: AssemblyInformationalVersion("1.0.5.2")]
